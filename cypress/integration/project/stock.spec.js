9162d178fbb19680d0a78a517d7fe2bb1372e33e
matias <matias.ferron@ing.austral.edu.ar>
2021-05-31 16:09:17 -0300

    enzyme and initial comment for test

---
+++ cypress/integration/project/stock.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+// Criterios de aceptación: 1, 2, 3, 4, 5, 6
+// // Se puede apretar cualquier acción en la lista de mis acciones para poder ser visualizada.
+// // 1 - Ingresar a la plataforma.
+// // 2 - Presionar sobre una acción cualquiera en la lista.
+// // -  La pantalla con toda la información de la acción seleccionada.
+// // - La información de la acción  cumple con los subitems del criterio especificado.
+
+//Criterios de aceptación: 7
+// Se puede apretar la cruz para cerrar la visualización de la acción.
+// 1 - Ingresar a la plataforma.
+// 2 - Presionar sobre una acción cualquiera de la lista.
+// 3 - Presionar sobre la cruz de arriba a la derecha para cerrar la visualización.
+// - Volver a la visualización de mi portfolio.
+
+// Criterios de aceptación: 4
+// Se puede cambiar para visualizar la variación en la cotización por semana, mes, seis meses y un año.
+//
+// 1 - Ingresar a la plataforma .
+// 2 - Presionar sobre una acción cualquiera de la lista.
+// 3 - Presionar sobre el tab para ver el gráfico por semana.
+// 4 - Presionar sobre el tab para ver el gráfico por mes.
+// 5 - Presionar sobre el tab para ver el gráfico por seis meses.
+// 6 - Presionar sobre el tab para ver el gráfico por un año.
+// - Los datos del gráfico deben ser acorde a la información solicitada.
--- src/stock/components/Stock/Stock.jsx
+++ src/stock/components/Stock/Stock.jsx
@@ -6,12 +6,53 @@ import './Stock.scss';
  * @description
  * @param { object } props no redux
  * @return { * } component
+ * 1 - Debe mostrar la cotización de la acción en el momento con la diferencia del día.  A la derecha debe mostrar el nombre de la empresa.
+ 2 - La diferencia de rentabilidad en el día estará en color:
+         a - Rojo: En el caso que el porcentaje de rentabilidad sea negativo.
+         b - Verde: En el caso que el porcentaje de rentabilidad sea positivo.
+         c -  Gris: En el caso que el porcentaje de rentabilidad sea cero.
+ 3 - Se debe mostrar la cotización actual de la acción.
+ 4 - Se debe mostrar un gráfico con la variación diaria en la cotización. Debe tener la opción de pedir la visualización de la variación en la cotización por semana, por mes, por medio año y un año.
+ 5- Debe haber una tabla con las estadísticas de la acción.
+         a - Open: el valor con el que abrió la acción en la bolsa.
+         b - High: el valor máximo que tomó la acción en el día.
+         c - Low: el valor mínimo que tomó la acción en el día.
+         d - 52wk high: el valor máximo que tomó la acción en las últimas 52 semanas.
+         e - 52wk low: el valor mínimo que tomó la acción en las últimas 52 semanas.
+         f - Volume: la cantidad de títulos negociados de la acción en el día.
+         g - Avg volume: el volumen medio de las acciones negociadas en las últimas 52 semanas.
+         h - Mkt cap: la dimensión económica de la empresa.
+         i - P/E ratio: el ratio de la empresa (la cotización del momento de la empresa dividido las ganancias reportadas en los últimos cuatro trimestres).
+         j - Div/Yield: el rendimiento por dividendo.
+ 6- Debe aparecer un cuadro con información de la empresa.
+ 7- Arriba deberá aparecer una cruz para salir de la visualización de la acción específica.
+
+
  */
 const Stock = (props) => {
     const {
 
     } = props;
 
+    const data = [
+        {
+            companyName: 'tesla',
+            companyDescription: 'Electrical inc',
+            dayProfit:5,
+            price: 55,
+            historyData: {week:[45,37,29,60], year:[20,44,45,37,29,60,20,44,45,37,29,60]},
+            openValue: 50,
+            dayHigh: 60,
+            dayLow: 40,
+            yearHigh: 70,
+            yearLow:20,
+            volume: 20,
+            avg_volume: 30,
+            mkt_cap: 10,
+            pe_ratio: 30,
+            div_Yield: 20,
+        }
+    ]
     return (
         <div>
             <p>{'This is Stock'}</p>
