bdd75b421c82fa261b710ae265a6fdacaa74aa55
matias <matias.ferron@ing.austral.edu.ar>
2021-06-02 09:52:02 -0300

    home path

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import SharesList from "./base/components/SharesList/SharesList";
 import Stock from "./stock/components/Stock/Stock";
+import { SearchBar } from "./base/components/SearchBar/SearchBar";
 
 function App() {
   return (
     <div>
+      <SearchBar />
       <Switch>
+        <Route exact path={"/"} render={(): JSX.Element => <SharesList />} />
         <Route
           exact
-          path={"/"}
+          path={"/stock"}
           render={(): JSX.Element => (
             <Stock showChart={true} stockBasicData={{ stockSymbol: "TSLA" }} />
           )}
-        />
-        <Route
-          exact
-          path={"/list"}
-          render={(): JSX.Element => <SharesList />}
         />
       </Switch>
     </div>
