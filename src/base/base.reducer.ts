6d1a6d9af561cd031073053781f87b8b39ad8349
joacoiannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2021-06-01 22:14:03 -0300

    trying

--- src/base/base.reducer.ts
+++ src/base/base.reducer.ts
@@ -1,19 +1,18 @@
-import baseActions, {
-    // [MODULE REDUCER] IMPORT ACTIONS
-} from './base.actions';
-import {ActionTypes} from "../redux/actions";
-import  {BaseState} from "./base.actions"
-import {REQUEST_STATUS} from "../utils/consts";
+import baseActions from "./base.actions"; // [MODULE REDUCER] IMPORT ACTIONS
+import { ActionTypes } from "../redux/actions";
+import { BaseState } from "./base.actions";
+import { REQUEST_STATUS } from "../utils/consts";
 
 const initialState = {
-    // [MODULE REDUCER] INITIAL STATE
+  // [MODULE REDUCER] INITIAL STATE
 };
 
 const baseReducer = (state = initialState, action: ActionTypes): BaseState => {
-    switch (action.type) {
-        // [MODULE REDUCER] SWITCH CASE
-        default: return state;
-    };
-}
+  switch (action.type) {
+    // [MODULE REDUCER] SWITCH CASE
+    default:
+      return state;
+  }
+};
 
 export default baseReducer;
